eb9bc2f090cfc86168a525b0c8c45859dcfbf4d8
Namit <namit.ohri@gmail.com>
2015-03-26 18:33:25 +0530

    namit@master:data extracted in json format

--- iitnews/spiders/toi_spider.py
+++ iitnews/spiders/toi_spider.py
@@ -6,10 +6,26 @@ class ToiSpider(scrapy.Spider):
   name ="toi"
   allowed_domains = ["timesofindia.indiatimes.com"]
   start_urls = [
-    "http://timesofindia.indiatimes.com/topic/IIT-Bombay"
+    "http://timesofindia.indiatimes.com/topic/IIT-Bombay",
+'''    "http://timesofindia.indiatimes.com/topic/IIT-Delhi",
+    "http://timesofindia.indiatimes.com/topic/IIT-Madras",
+    "http://timesofindia.indiatimes.com/topic/IIT-Kharagpur",
+    "http://timesofindia.indiatimes.com/topic/IIT-Kanpur",
+    "http://timesofindia.indiatimes.com/topic/IIT-Roorkee",
+    "http://timesofindia.indiatimes.com/topic/IT-BHU",'''
   ]
   def parse(self, response):
     filename = "toi_" + response.url.split("/")[-1]
-    with open(filename, 'wb') as f:
-      f.write(response.body)
+    news_list = response.xpath('//div[@class="topicsnews-box clearfix"]')
+    for news in news_list:
+      news_headline = news.xpath('.//strong/text()').extract()
+      news_synopsis = news.xpath('.//span[@class="topicnews-synopsis"]/text()').extract()
+      news_link = news.xpath('.//a/@href').extract()
+      item = IitNewsItem()
+      item['headline'] = news_headline[0]
+      item['synopsis'] = news_synopsis[0]
+      item['link'] = news_link[0]
+      yield item
+      #with open(filename, 'wb') as f:
+      #  f.write(item)
 
