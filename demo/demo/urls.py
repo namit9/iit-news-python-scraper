72bf7bbe5bf781f3d8757e641cb54a71503d2ee5
Namit Ohri <namit.ohri@gmail.com>
2015-05-19 17:23:26 +0530

    namit@master:api support added

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 
 import views
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^news/$', views.index, name='index'),
+    url(r'^api/$', views.api, name='api'),
 ]
