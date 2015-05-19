334912899c1cee579e806da99103a5c797b1af58
Namit Ohri <namit@Namits-MacBook-Air.local>
2015-05-19 17:28:00 +0530

    namit@master:api support added

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
 
+import json
+
 def index(request):
-  f = open("../toi_organised.json","r")
+  f = open("./../toi_organised.json","r")
   return HttpResponse(f, content_type="application/json")
+
+def api(request):
+  list_iits = request.GET.getlist('iits', '')
+  count = int(request.GET.get('items', -1))
+  with open("./../toi_organised.json", "r") as f:
+    data = json.load(f)
+  result = {}
+  for iit in list_iits:
+    if count > len(data[iit]) or count == -1:
+      result[iit] = data[iit]
+    else:
+      result[iit] = data[iit][:count-1]
+    
+  return HttpResponse(json.dumps(result), content_type="application/json")
